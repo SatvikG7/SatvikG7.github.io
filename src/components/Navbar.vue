09a61e4560edfacae624a4b0ebbf855225a1d6c2
Satvik Gaikwad <satvik.g.code@gmail.com>
2021-04-15 16:43:02 +0530

    minor --navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -170,7 +170,7 @@ nav {
   #home {
     position: absolute;
     left: 5px;
-    top: 10px;
+    top: 5px;
     bottom:0px;
     font-size: 30px;
     font-weight: 800;
@@ -179,7 +179,7 @@ nav {
   #close {
     position: absolute;
     left: 5px;
-    top: 10px;
+    top: 5px;
     bottom:0px;
     font-size: 30px;
     font-weight: 800;
